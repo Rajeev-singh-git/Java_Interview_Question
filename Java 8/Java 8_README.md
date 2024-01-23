e2d8d224b3fa93f4132a588d8eb65ad3fed246eb
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-01-23 18:52:55 +0530

    Code without lambda expression

---
+++ Java 8/Java 8_README.md
@@ -0,0 +1,14 @@
+# Java Interview Questions
+
+## Polymorphism concept
+
+In an interface, the JVM determines the exact method to call based on the object's type at runtime, not the reference type.
+
+```java
+Interf I = new Demo();
+I.methodOne();  // This calls the methodOne implementation in Demo class
+
+Interf I2 = new Demo2();
+I2.methodOne();  // This calls the methodOne implementation in Demo2 class
+```
+
---
+++ Java 8/src/Interf.java
@@ -0,0 +1,32 @@
+public interface Interf {
+
+    public void methodOne();
+}
+
+class Demo implements Interf{
+
+    @Override
+    public void methodOne() {
+        System.out.println("Method one executed");             // Method one executed
+    }
+}
+
+class Demo2 implements Interf{
+
+    @Override
+    public void methodOne() {
+        System.out.println("Method one executed from Demo 2 class");    // Method one executed from Demo 2 class
+    }
+}
+
+class Test{
+
+    public static void main(String[]args){
+        Interf I = new Demo();
+        I.methodOne();
+
+        Interf I2 = new Demo2();
+        I2.methodOne();
+
+    }
+}
