83637a7111553d740aff6f20814fb694029c593d
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-01-25 00:28:05 +0530

    Collection

---
+++ Collections/src/List/src/ArrayList/IterateArrayList.java
@@ -0,0 +1,72 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.*;
+
+// Write a Java program to iterate through all elements in an array list.
+// Retrieve an element (at a specified index) from a given array list.
+public class IterateArrayList {
+
+    public static void main(String[]args){
+
+        ArrayList<String> colorList = new ArrayList<>();
+
+        colorList.add("Red");
+        colorList.add("Blue");
+        colorList.add("Black");
+
+       System.out.println("***********Iterating ArrayList**********");
+
+       for(String color : colorList){
+           System.out.println(color);
+       }
+
+
+
+       colorList.add(0,"Pink");
+
+        System.out.println("*******************Iterating ArrayList after adding**************************");
+
+        for(String color : colorList){
+            System.out.println(color);
+        }
+
+        System.out.println("*******************Retrieve an element at index 2**************************");
+        // retrieve an element (at a specified index) from a given array list.
+        System.out.println(colorList.get(2));
+
+
+
+        System.out.println("******************* update an array element by the given element **************************");
+
+        colorList.set(2,"Brown");
+        System.out.println(colorList);
+
+
+
+        System.out.println("******************* remove the third element from an array list **************************");
+        colorList.remove(3);
+        System.out.println(colorList);
+
+
+
+        System.out.println("*******************  Search the value Red **************************");
+        if(colorList.contains("Red")){
+            System.out.println("Found");
+        }else{
+            System.out.println("Not Found");
+        }
+
+        System.out.println("*******************  Sort array list  **************************");
+        Collections.sort(colorList);
+        System.out.println(colorList);
+
+        System.out.println("*******************  Copy one array list into another  **************************");
+
+
+
+    }
+
+
+}
---
+++ Collections/src/List/src/ArrayList/createArrayList1.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+//Write a Java program to create an array list, add some colors (strings) and print out the collection.
+public class createArrayList1 {
+
+    public static void main(String[]args){
+
+        ArrayList<String> colorList = new ArrayList<>();
+
+        colorList.add("Red");
+        colorList.add("Blue");
+        colorList.add("Black");
+
+        System.out.println(colorList);
+
+    }
+
+}
